2533438d11dac98d34d1bcb5e1779c058cd46c1e
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-01-25 00:30:07 +0530

    Added Database manager page

--- Sentinnel/AdminPage.Designer.vb
+++ Sentinnel/AdminPage.Designer.vb
@@ -45,9 +45,27 @@ Partial Class AdminPage
         Me.Label7 = New System.Windows.Forms.Label()
         Me.Action_taken = New System.Windows.Forms.RichTextBox()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.VirusName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VirusHash = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.VirusType = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Username = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Password = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.JoinDate = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FirstName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LastName = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Panel2.SuspendLayout()
         Me.Panel1.SuspendLayout()
         CType(Me.Icon_picturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Panel2
@@ -169,7 +187,7 @@ Partial Class AdminPage
         '
         Me.User_title.AutoSize = True
         Me.User_title.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.User_title.Location = New System.Drawing.Point(216, 60)
+        Me.User_title.Location = New System.Drawing.Point(16, 12)
         Me.User_title.Name = "User_title"
         Me.User_title.Size = New System.Drawing.Size(150, 25)
         Me.User_title.TabIndex = 66
@@ -179,28 +197,28 @@ Partial Class AdminPage
         '
         Me.ComboBox1.FormattingEnabled = True
         Me.ComboBox1.Items.AddRange(New Object() {"User1", "User2", "User3", "User4"})
-        Me.ComboBox1.Location = New System.Drawing.Point(664, 63)
+        Me.ComboBox1.Location = New System.Drawing.Point(464, 15)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
         Me.ComboBox1.TabIndex = 67
         '
         'Date_box
         '
-        Me.Date_box.Location = New System.Drawing.Point(358, 119)
+        Me.Date_box.Location = New System.Drawing.Point(158, 71)
         Me.Date_box.Name = "Date_box"
         Me.Date_box.Size = New System.Drawing.Size(100, 20)
         Me.Date_box.TabIndex = 68
         '
         'File_count
         '
-        Me.File_count.Location = New System.Drawing.Point(358, 160)
+        Me.File_count.Location = New System.Drawing.Point(158, 112)
         Me.File_count.Name = "File_count"
         Me.File_count.Size = New System.Drawing.Size(100, 20)
         Me.File_count.TabIndex = 69
         '
         'Inf_score
         '
-        Me.Inf_score.Location = New System.Drawing.Point(358, 201)
+        Me.Inf_score.Location = New System.Drawing.Point(158, 153)
         Me.Inf_score.Name = "Inf_score"
         Me.Inf_score.Size = New System.Drawing.Size(100, 20)
         Me.Inf_score.TabIndex = 70
@@ -209,7 +227,7 @@ Partial Class AdminPage
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(217, 119)
+        Me.Label3.Location = New System.Drawing.Point(17, 71)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(44, 20)
         Me.Label3.TabIndex = 71
@@ -219,7 +237,7 @@ Partial Class AdminPage
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(217, 160)
+        Me.Label4.Location = New System.Drawing.Point(17, 112)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(110, 20)
         Me.Label4.TabIndex = 72
@@ -229,7 +247,7 @@ Partial Class AdminPage
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(217, 201)
+        Me.Label5.Location = New System.Drawing.Point(17, 153)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(117, 20)
         Me.Label5.TabIndex = 73
@@ -239,7 +257,7 @@ Partial Class AdminPage
         '
         Me.Virus_title.AutoSize = True
         Me.Virus_title.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Virus_title.Location = New System.Drawing.Point(216, 259)
+        Me.Virus_title.Location = New System.Drawing.Point(16, 211)
         Me.Virus_title.Name = "Virus_title"
         Me.Virus_title.Size = New System.Drawing.Size(154, 25)
         Me.Virus_title.TabIndex = 74
@@ -247,7 +265,7 @@ Partial Class AdminPage
         '
         'Virus_found
         '
-        Me.Virus_found.Location = New System.Drawing.Point(221, 336)
+        Me.Virus_found.Location = New System.Drawing.Point(21, 288)
         Me.Virus_found.Name = "Virus_found"
         Me.Virus_found.Size = New System.Drawing.Size(279, 96)
         Me.Virus_found.TabIndex = 75
@@ -257,7 +275,7 @@ Partial Class AdminPage
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label7.Location = New System.Drawing.Point(217, 313)
+        Me.Label7.Location = New System.Drawing.Point(17, 265)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(112, 20)
         Me.Label7.TabIndex = 76
@@ -265,7 +283,7 @@ Partial Class AdminPage
         '
         'Action_taken
         '
-        Me.Action_taken.Location = New System.Drawing.Point(506, 336)
+        Me.Action_taken.Location = New System.Drawing.Point(306, 288)
         Me.Action_taken.Name = "Action_taken"
         Me.Action_taken.Size = New System.Drawing.Size(279, 96)
         Me.Action_taken.TabIndex = 77
@@ -275,11 +293,124 @@ Partial Class AdminPage
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(502, 313)
+        Me.Label2.Location = New System.Drawing.Point(302, 265)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(102, 20)
         Me.Label2.TabIndex = 78
         Me.Label2.Text = "Action Taken"
+        '
+        'Panel3
+        '
+        Me.Panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel3.Controls.Add(Me.Panel4)
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Controls.Add(Me.User_title)
+        Me.Panel3.Controls.Add(Me.Action_taken)
+        Me.Panel3.Controls.Add(Me.ComboBox1)
+        Me.Panel3.Controls.Add(Me.Label7)
+        Me.Panel3.Controls.Add(Me.Date_box)
+        Me.Panel3.Controls.Add(Me.Virus_found)
+        Me.Panel3.Controls.Add(Me.File_count)
+        Me.Panel3.Controls.Add(Me.Virus_title)
+        Me.Panel3.Controls.Add(Me.Inf_score)
+        Me.Panel3.Controls.Add(Me.Label5)
+        Me.Panel3.Controls.Add(Me.Label3)
+        Me.Panel3.Controls.Add(Me.Label4)
+        Me.Panel3.Location = New System.Drawing.Point(200, 47)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(601, 403)
+        Me.Panel3.TabIndex = 79
+        '
+        'Panel4
+        '
+        Me.Panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel4.Controls.Add(Me.DataGridView2)
+        Me.Panel4.Controls.Add(Me.DataGridView1)
+        Me.Panel4.Controls.Add(Me.Label8)
+        Me.Panel4.Controls.Add(Me.Label10)
+        Me.Panel4.Location = New System.Drawing.Point(-1, -1)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(601, 403)
+        Me.Panel4.TabIndex = 80
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(16, 12)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(147, 25)
+        Me.Label8.TabIndex = 66
+        Me.Label8.Text = "Virus Databse"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(16, 211)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(155, 25)
+        Me.Label10.TabIndex = 74
+        Me.Label10.Text = "User Database"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.VirusName, Me.VirusHash, Me.VirusType})
+        Me.DataGridView1.Location = New System.Drawing.Point(21, 53)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(557, 138)
+        Me.DataGridView1.TabIndex = 79
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Username, Me.Password, Me.JoinDate, Me.FirstName, Me.LastName})
+        Me.DataGridView2.Location = New System.Drawing.Point(21, 254)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(557, 138)
+        Me.DataGridView2.TabIndex = 80
+        '
+        'VirusName
+        '
+        Me.VirusName.HeaderText = "Virus Name"
+        Me.VirusName.MinimumWidth = 8
+        Me.VirusName.Name = "VirusName"
+        '
+        'VirusHash
+        '
+        Me.VirusHash.HeaderText = "Virus Hash"
+        Me.VirusHash.Name = "VirusHash"
+        '
+        'VirusType
+        '
+        Me.VirusType.HeaderText = "Virus Type"
+        Me.VirusType.Name = "VirusType"
+        '
+        'Username
+        '
+        Me.Username.HeaderText = "Username"
+        Me.Username.Name = "Username"
+        '
+        'Password
+        '
+        Me.Password.HeaderText = "Password"
+        Me.Password.Name = "Password"
+        '
+        'JoinDate
+        '
+        Me.JoinDate.HeaderText = "Join Date"
+        Me.JoinDate.Name = "JoinDate"
+        '
+        'FirstName
+        '
+        Me.FirstName.HeaderText = "First Name"
+        Me.FirstName.Name = "FirstName"
+        '
+        'LastName
+        '
+        Me.LastName.HeaderText = "Last Name"
+        Me.LastName.Name = "LastName"
         '
         'AdminPage
         '
@@ -287,19 +418,7 @@ Partial Class AdminPage
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Action_taken)
-        Me.Controls.Add(Me.Label7)
-        Me.Controls.Add(Me.Virus_found)
-        Me.Controls.Add(Me.Virus_title)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Inf_score)
-        Me.Controls.Add(Me.File_count)
-        Me.Controls.Add(Me.Date_box)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.User_title)
+        Me.Controls.Add(Me.Panel3)
         Me.Controls.Add(Me.Window_Button)
         Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.Panel2)
@@ -310,8 +429,13 @@ Partial Class AdminPage
         Me.Panel1.ResumeLayout(False)
         Me.Panel1.PerformLayout()
         CType(Me.Icon_picturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Panel2 As Panel
@@ -337,4 +461,18 @@ Partial Class AdminPage
     Friend WithEvents Label7 As Label
     Friend WithEvents Action_taken As RichTextBox
     Friend WithEvents Label2 As Label
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents DataGridView2 As DataGridView
+    Friend WithEvents Username As DataGridViewTextBoxColumn
+    Friend WithEvents Password As DataGridViewTextBoxColumn
+    Friend WithEvents JoinDate As DataGridViewTextBoxColumn
+    Friend WithEvents FirstName As DataGridViewTextBoxColumn
+    Friend WithEvents LastName As DataGridViewTextBoxColumn
+    Friend WithEvents VirusName As DataGridViewTextBoxColumn
+    Friend WithEvents VirusHash As DataGridViewTextBoxColumn
+    Friend WithEvents VirusType As DataGridViewTextBoxColumn
 End Class
