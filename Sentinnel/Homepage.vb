cd13e35bf69ad1a11458318218215a3ae9c1d51c
Joe Sanjo <dedsec8.0.8.0x00@gmail.com>
2024-02-27 02:33:22 +0530

    database connection

--- Sentinnel/Homepage.vb
+++ Sentinnel/Homepage.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.Runtime.InteropServices
+Imports System.Data.SqlClient
 
 Public Class Homepage
 
@@ -6,6 +7,7 @@ Public Class Homepage
     Public Const WM_NCLBUTTONDOWN As Integer = &HA1
     Public Const HT_CAPTION As Integer = &H2
     Public lastFormLocation As Point
+    Dim connectionString As String = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=D:\--PROJECTS2024--\SENTINNEL\SENTINNEL\BIN\DEBUG\SENTINNELDB.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False"
 
     <DllImportAttribute("user32.dll")>
     Public Shared Function SendMessage(ByVal hWnd As IntPtr, ByVal Msg As Integer, ByVal wParam As Integer, ByVal lParam As Integer) As Integer
@@ -56,18 +58,37 @@ Public Class Homepage
     End Sub
 
     Private Sub Login_button_Click(sender As Object, e As EventArgs) Handles Login_button.Click
-        'Database code to check with the username/userID in the database and then log in
-        If Login_username.Text = "Toad" And Login_password.Text = "Toad" Then
-            Me.Hide()
-            Form1.Show()
-            Form1.Location = New Point(Me.Location)
-        ElseIf Login_username.Text = "Admin" And Login_password.Text = "Admin" And Admin_key.Text = "Admin" Then
-            Me.Hide()
-            AdminPage.Show()
-            AdminPage.Location = New Point(Me.Location)
-        Else
-            MsgBox("Incorrect Credentials")
-        End If
+
+
+        'idk man login shit
+        Dim username As String = Login_username.Text
+        Dim password As String = Login_password.Text
+
+        Using connection As New SqlConnection(connectionString)
+            Try
+                connection.Open()
+
+                Dim query As String = "SELECT COUNT(*) FROM UserDB WHERE Username = @Username AND Password = @Password"
+                Using command As New SqlCommand(query, connection)
+                    command.Parameters.AddWithValue("@Username", username)
+                    command.Parameters.AddWithValue("@Password", password)
+
+                    Dim count As Integer = Convert.ToInt32(command.ExecuteScalar())
+
+                    If count > 0 Then
+                        ' User exists with the provided username and password
+                        ' Proceed with login
+                        Me.Hide()
+                        Form1.Show()
+                        Form1.Location = New Point(Me.Location)
+                    Else
+                        MsgBox("Incorrect Credentials" + vbCrLf + username + vbCrLf + password)
+                    End If
+                End Using
+            Catch ex As Exception
+                MessageBox.Show("Error: " & ex.Message)
+            End Try
+        End Using
     End Sub
 
     Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel1.MouseDown
